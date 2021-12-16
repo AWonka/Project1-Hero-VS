12e193fed039f064dd4d4795b506c9a44977a910
Dasia Smith <dasiasmith@Dasias-Air.attlocal.net>
2021-12-16 16:50:00 -0600

    Edited html, added fight html, and fight js, edited css/foundation styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,16 @@ var teamDC = document.querySelector('#dcTeamNames');
 
 var parsed = "";
 const superAPIKey = '10215957904298742';
-const newKey = 'k_22ot0u2y/';
+const newKey = 'k_9qqhtsbd/';
 var imdbAPIKey = 'k_rjtma7sz/'
 
+//The number associated with the position in the html will be appended to the superhero API as the hero id
 $(marvelName).change(function() {
     let position = this.value;
     let idURL = 'https://www.superheroapi.com/api.php/10215957904298742/' + position;
 
+        //Each if statement takes the user's hero selection and adds the movie selection associated with the hero
         if (this.value == 149) {
-            // clearMarvMovie();
             let select = document.getElementById('marvArray');
             select.innerHTML = '';
             let elmts = ["Captain America: The First Avenger", "Captain America: The Winter Soldier", "Captain America: Civil War"];
@@ -33,10 +34,10 @@ $(marvelName).change(function() {
             let el = document.createElement('option');
             el.textContent = optn;
             el.value = optn;
-            select.appendChild(el);
-            
+            select.appendChild(el); 
         }
-            //replaces globally
+            //Replaces every space globally with %20 to be used for the API url
+            //Calls the getRatingAPI to get movie rating for selected hero movie
             let result = elmts[0].replace(/ /g, "%20");
             console.log(result);
             getMRatingAPI(result);
@@ -187,8 +188,10 @@ $(marvelName).change(function() {
             console.log(result);
             getMRatingAPI(result);
         };
+        //Calls API using the url created in the above if statements based on selection
         getMarvelAPI(idURL);
 });
+//The imdb API is called to find the ratings based on the user's selected movie choice
 $(movieChoice).change(function() {
     console.log(this.value);
     let result = this.value.replace(/ /g, "%20");
@@ -201,7 +204,6 @@ $(dcName).change(function() {
     let idURL = 'https://www.superheroapi.com/api.php/10215957904298742/' + position;
 
     if (this.value == 69) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Batman(1989)", "Batman Returns", "Batman & Robin", "Batman Forever", "Batman V Superman: Dawn of Justice", "Batman Begins", "The Dark Knight", "The Dark Knight Rises", "Justice League"];
@@ -218,7 +220,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 644) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Superman The Movie", "Superman II", "Superman III", "Superman IV: The Quest for Peace", "Superman Returns", "Man of Steel", "Batman V Superman: Dawn of Justice", "Justice League"];
@@ -235,7 +236,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 306) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Green Lantern"];
@@ -252,7 +252,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 720) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Wonder Woman", "Wonder Woman 1984", "Justice League"];
@@ -269,7 +268,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 194) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Justice League"];
@@ -286,7 +284,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 370) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Joker", "Suicide Squad", "Batman"];
@@ -303,7 +300,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 309) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Birds of Prey", "Suicide Squad", "The Suicide Squad"];
@@ -320,7 +316,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 367) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Constantine"];
@@ -337,7 +332,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 165) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["CatWoman", "The Dark Knight Rises"];
@@ -354,7 +348,6 @@ $(dcName).change(function() {
         getDCRatingAPI(dcresult);
 
     } else if (this.value == 38) {
-        // clearMarvMovie();
         let select = document.getElementById('dcArray');
         select.innerHTML = '';
         let elmts = ["Aquaman", "Justice League"];
@@ -370,7 +363,7 @@ $(dcName).change(function() {
         console.log(dcresult);
         getDCRatingAPI(dcresult);
     };
-
+    
     getDCAPI(idURL);
 });
 $(dcmovieChoice).change(function() {
@@ -379,7 +372,7 @@ $(dcmovieChoice).change(function() {
             console.log(dcresult);
             getDCRatingAPI(dcresult);
 })
-
+//Grabs powerstats from superhero API and creates an element for each to show in the html
 function getMarvelAPI(link) {
     fetch(link)
         .then(res => res.json())
@@ -398,6 +391,8 @@ function getMarvelAPI(link) {
         var mDur = document.createElement("p")
         var mPower = document.createElement("p")
         var mCombat = document.createElement("p")
+        var mRate = document.createElement("p")
+        mRate.setAttribute('id', 'dummyRate')
 
         marvTitle.textContent = data.name
         marvImg.setAttribute("src", data.image.url);
@@ -410,6 +405,7 @@ function getMarvelAPI(link) {
         mDur.textContent = 'Durability: ' + data.powerstats.durability;
         mPower.textContent = 'Power: ' + data.powerstats.power;
         mCombat.textContent = 'Combat: ' + data.powerstats.combat;
+        mRate.textContent = 'Movie Rating: ';
 
        marvCharTitle.appendChild(marvTitle);
        mdivEl.appendChild(marvImg);
@@ -420,8 +416,10 @@ function getMarvelAPI(link) {
        mdivEl.appendChild(mDur);
        mdivEl.appendChild(mPower);
        mdivEl.appendChild(mCombat);
+       mdivEl.appendChild(mRate);
        marvStats.appendChild(mdivEl);
        
+       //Outputs the sum and averages of the powerstats + ratings
        var mstatTot = Object.values(data.powerstats)
        var mSum = 0;
        for( var i = 0; i < mstatTot.length; i++ ){
@@ -430,7 +428,8 @@ function getMarvelAPI(link) {
        var mAvg = Math.round(mSum/mstatTot.length);
        console.log( "The sum of all the elements is: " + mSum + " The average is: " + mAvg );
 
-
+       //Stores the hero names in an array and pushes the first name out of the array one the length is equal to 3
+       //The names are stored on the html sheet as the individuals in the Hero team
        var allSelectedChars = [];
        if (localStorage.getItem('marvSelectedChars')){
         allSelectedChars = JSON.parse(localStorage.getItem('marvSelectedChars'))
@@ -453,7 +452,7 @@ function getMarvelAPI(link) {
            var nameDisplay = allSelectedChars[x];
            var mStored = document.createElement('p');
            mStored.textContent = nameDisplay;
-           mStored.setAttribute('style', 'color:red')
+           mStored.setAttribute('style', 'color:black')
            teamMarvel.append(mStored);
        }
     })
@@ -463,6 +462,7 @@ function getMarvelAPI(link) {
 
     
 }
+//Get DC Movie data
 function getDCAPI(link) {
     fetch(link)
     .then(res => res.json())
@@ -481,6 +481,8 @@ function getDCAPI(link) {
         var dcDur = document.createElement("p")
         var dcPower = document.createElement("p")
         var dcCombat = document.createElement("p")
+        var dcRate = document.createElement("p")
+        dcRate.setAttribute('id', 'dcDummyRate')
 
         dcTitle.textContent = data.name
         dcImg.setAttribute("src", data.image.url);
@@ -493,6 +495,7 @@ function getDCAPI(link) {
         dcDur.textContent = 'Durability: ' + data.powerstats.durability;
         dcPower.textContent = 'Power: ' + data.powerstats.power;
         dcCombat.textContent = 'Combat: ' + data.powerstats.combat;
+        dcRate.textContent = 'Movie Rating: ';
         
         dcCharTitle.appendChild(dcTitle);
         dcDivEl.appendChild(dcImg);
@@ -503,6 +506,7 @@ function getDCAPI(link) {
         dcDivEl.appendChild(dcDur);
         dcDivEl.appendChild(dcPower);
         dcDivEl.appendChild(dcCombat);
+        dcDivEl.appendChild(dcRate);
         dcStats.appendChild(dcDivEl);
 
         var dstatTot = Object.values(data.powerstats)
@@ -536,7 +540,7 @@ function getDCAPI(link) {
            var nameDisplay = allSelectedChars[x];
            var dcStored = document.createElement('p');
            dcStored.textContent = nameDisplay;
-           dcStored.setAttribute('style', 'color:blue')
+           dcStored.setAttribute('style', 'color:black')
            teamDC.append(dcStored);
        } 
 
@@ -545,10 +549,7 @@ function getDCAPI(link) {
     dcCharTitle.innerHTML = "";
     dcStats.innerHTML = "";
     teamDC.innerHTML = "";
-    console.log(totalDCStatArray)
 }
-
-clear(myChart)
 
 //Getting movie ratings
 function getMRatingAPI(marvMovie) {
@@ -570,6 +571,8 @@ function getMRatingAPI(marvMovie) {
                                 var mrating = parseInt(data.metacritic)
                             }
                             console.log(mrating);
+                            var nodelist = document.getElementById("dummyRate")
+                            nodelist.textContent = 'Movie Rating: ' + mrating
 
                             var mRateArray = [];
                             if (localStorage.getItem('marvSelectedRatings')) {
@@ -580,35 +583,6 @@ function getMRatingAPI(marvMovie) {
                             }
                             mRateArray.push(mrating)
                             localStorage.setItem("marvSelectedRatings", JSON.stringify(mRateArray));
-
-                            const labels = [
-                                'Marvel Powers',
-                                'Marvel Movie',
-                                'DC Powers',
-                                'DC Movie',
-                              ];
-                              const info = {
-                                labels: labels,
-                                datasets: [{
-                                  label: 'Marvel vs DC',
-                                  backgroundColor: 'rgb(255, 99, 132)',
-                                  borderColor: 'rgb(255, 99, 132)',
-                                  data: [mrating],
-                                }]
-                              };
-                            
-                              const config = {
-                                type: 'bar',
-                                data: data,
-                                options: {}
-                              };
-                            
-                              const myChart = new Chart(
-                                document.getElementById('myChart'),
-                                myChart.append(myChart),
-                                config
-                              );
-                              
                         })
                     }
                 })
@@ -616,8 +590,6 @@ function getMRatingAPI(marvMovie) {
         }
     })
 }
-
-
 
 function getDCRatingAPI(dcMovie) {
     fetch('https://imdb-api.com/en/API/SearchMovie/'+ newKey + dcMovie)
@@ -638,6 +610,8 @@ function getDCRatingAPI(dcMovie) {
                                 var dcrating = parseInt(data.metacritic)
                             }
                             console.log(dcrating);
+                            var dcnodelist = document.getElementById("dcDummyRate")
+                            dcnodelist.textContent = 'Movie Rating: ' + dcrating
 
                             var dcRateArray = [];
                             if (localStorage.getItem('dcSelectedRatings')) {
@@ -648,33 +622,6 @@ function getDCRatingAPI(dcMovie) {
                             }
                             dcRateArray.push(dcrating)
                             localStorage.setItem("dcSelectedRatings", JSON.stringify(dcRateArray));
-
-                            const labels = [
-                                'Marvel Powers',
-                                'Marvel Movie',
-                                'DC Powers',
-                                'DC Movie',
-                              ];
-                              const info = {
-                                labels: labels,
-                                datasets: [{
-                                  label: 'Marvel vs DC',
-                                  backgroundColor: 'rgb(255, 99, 132)',
-                                  borderColor: 'rgb(255, 99, 132)',
-                                  data: [dcrating],
-                                }]
-                              };
-                            
-                              const config = {
-                                type: 'bar',
-                                data: data,
-                                options: {}
-                              };
-                            
-                              const myChart = new Chart(
-                                document.getElementById('myChart'),
-                                config
-                              );
                         })
                     }
                 })
@@ -683,18 +630,20 @@ function getDCRatingAPI(dcMovie) {
     })
 }
 
+//Gets the average from powerstats and average for movie rating
 var getmAvg = JSON.parse(localStorage.getItem('marvSelectedStats'))
 console.log(getmAvg);
 var getmRate = JSON.parse(localStorage.getItem('marvSelectedRatings'))
 console.log(getmRate)
 
+//Get total average of powerstats array and movie rating array
 var i, j;
 var mAverage = 0;
 
 for(i = 0; i < getmAvg.length; i++){
     mAverage += getmAvg[i];
 }
-for(j = 0; j < getmRate.length; j++){ // (two arrays for flexibility.)
+for(j = 0; j < getmRate.length; j++){ 
     mAverage += getmRate[j];
 }
 console.log(mAverage /= (i+j))
@@ -711,10 +660,28 @@ var dcAverage = 0;
 for(i = 0; i < getdcAvg.length; i++){
     dcAverage += getdcAvg[i];
 }
-for(j = 0; j < getdcRate.length; j++){ // (two arrays for flexibility.)
+for(j = 0; j < getdcRate.length; j++){ 
     dcAverage += getdcRate[j];
 }
 console.log(dcAverage /= (i+j))
 
+//From fight club html to display on winner screen
+var finalMarvAvg = document.getElementById('finalMarvAvg');
+var finalDCAvg = document.getElementById('finalDCAvg');
+var finalWinner = document.getElementById('finalWinner');
 
-  
+finalMarvAvg.textContent = mAverage.toFixed(1); //Using toFixed to display only one number after decimal
+console.log(finalMarvAvg)
+finalDCAvg.textContent = dcAverage.toFixed(1);
+getWinner(mAverage,dcAverage)
+
+function getWinner(m,dc) {
+    var highestScore = Math.max(m, dc)
+    if (m > dc) {
+        winner = 'Marvel'
+        finalWinner.textContent = winner
+    }else {
+        winner = 'DC'
+        finalWinner.textContent = winner
+    }
+}
